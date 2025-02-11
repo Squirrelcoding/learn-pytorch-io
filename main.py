287586fc71c2f08a183da98c4a09191ae39fce6c
squirrelcoding <rosilescarlos@gmail.com>
2025-02-10 22:01:21 -0600

    wefrtgr

--- main.py
+++ main.py
@@ -1,21 +1,49 @@
 import torch
 from torch import nn
-from torch.utils.data import DataLoader
-from torchvision import datasets
-from torchvision.transforms import ToTensor
+import matplotlib.pyplot as plt
 
-# Download training data from open datasets.
-training_data = datasets.FashionMNIST(
-    root="data",
-    train=True,
-    download=True,
-    transform=ToTensor(),
-)
+# Create known parameters
 
-# Download test data from open datasets.
-test_data = datasets.FashionMNIST(
-    root="data",
-    train=False,
-    download=True,
-    transform=ToTensor(),
-)
+weight = 0.7
+bias = 0.3
+
+start = 0
+end = 1
+step = 0.02
+X = torch.arange(start, end, step).unsqueeze(dim=1)
+y = weight * X + bias
+
+train_split = int(0.8 * len(X))
+X_train, y_train = X[:train_split], y[:train_split]
+X_test, y_test = X[train_split:], y[train_split:]
+
+def plot_predictions(train_data=X_train, 
+                     train_labels=y_train, 
+                     test_data=X_test, 
+                     test_labels=y_test, 
+                     predictions=None):
+    plt.figure(figsize=(10, 7))
+
+    # Plot the training data in blue
+    plt.scatter(train_data, train_labels, c="b", s=4, label="Training data")
+
+    # Plot the testing data in green
+    plt.scatter(test_data, test_labels, c="g", s=4, label="Testing data")
+
+    if predictions is not None:
+        plt.scatter(test_data, predictions, c="r", s=4, label="Predictions")
+
+    plt.legend(prop={"size":14})
+
+    plt.show()
+
+
+class LinearRegressionModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.weights = nn.Parameter(torch.randn(1, dtype=torch.float), requires_grad=True)
+        self.bias = nn.Parameter(torch.randn(1, dtype=torch.float), requires_grad=True)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.weights * x + self.bias
