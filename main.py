35554d4e60c587edaf6644bfcebcbb94e600360e
squirrelcoding <rosilescarlos28@gmail.com>
2025-02-19 23:24:35 -0600

    wedfr

--- main.py
+++ main.py
@@ -1,57 +1,17 @@
+# import pytorch stuff
 import torch
-
 from torch import nn
 
+# import torchvision stuff
+from torch.utils.data import DataLoader
 import torchvision
 from torchvision import datasets
 from torchvision.transforms import ToTensor
-from torch.utils.data import DataLoader
 
 import matplotlib.pyplot as plt
-from helper_functions import accuracy_fn
 
-from timeit import default_timer as timer
-from tqdm.auto import tqdm
 
-torch.manual_seed(42)
-
-def print_train_time(start: float, end: float, device: torch.device):
-    """Prints difference between start and end time.
-
-    Args:
-        start (float): Start time of computation (preferred in timeit format). 
-        end (float): End time of computation.
-        device ([type], optional): Device that compute is running on. Defaults to None.
-
-    Returns:
-        float: time between start and end in seconds (higher is longer).
-    """
-    total_time = end - start
-    print(f"Train time on {device}: {total_time:.3f} seconds")
-    return total_time
-
-def eval_model(model: torch.nn.Module,
-               data_loader: torch.utils.data.DataLoader,
-               loss_fn: torch.nn.Module,
-               accuracy_fn):
-    loss, acc = 0, 0
-    model.eval()
-    with torch.inference_mode():
-        for X, y in data_loader:
-            test_pred = model(X)
-
-            loss += loss_fn(test_pred, y)
-            acc += accuracy_fn(y_true=y, y_pred=test_pred.argmax(dim=1))
-
-        loss /= len(data_loader)
-        acc /= len(data_loader)
-    return {
-        "model_name": model.__class__.__name__,
-        "model_loss": loss,
-        "model_acc": acc
-    }
-
-train_data = datasets.FashionMNIST(
+train_data = datasets.MNIST(
     root="data",
     train=True,
     download=True,
@@ -59,64 +19,64 @@ train_data = datasets.FashionMNIST(
     target_transform=None
 )
 
-test_data = datasets.FashionMNIST(
+test_data = datasets.MNIST(
     root="data",
     train=False,
     download=True,
-    transform=ToTensor()
+    transform=ToTensor(),
+    target_transform=None
 )
 
-class_names = train_data.classes
+train_dataloader = DataLoader(
+    train_data,
+    batch_size=32,
+    shuffle=True,
+)
 
-fig = plt.figure(figsize=(9, 9))
-rows, cols = 4, 4
+test_dataloader = DataLoader(
+        test_data,
+        batch_size=32,
+        shuffle=False
+)
 
-# for i in range(1, rows * cols + 1):
-#     random_idx = torch.randint(0, len(train_data), size=[1]).item()
-#     img, label = train_data[random_idx] #pyright: ignore
-#     fig.add_subplot(rows, cols, i)
-#     plt.imshow(img.squeeze(), cmap="gray")
-#     plt.title(class_names[label])
-#     plt.axis(False)
-#
-# plt.show()
-
-BATCH_SIZE = 32
-
-train_dataloader = DataLoader(train_data, batch_size=BATCH_SIZE, shuffle=True)
-test_dataloader = DataLoader(test_data, batch_size=BATCH_SIZE, shuffle=False)
-
-
-class FashionMNISTModel(nn.Module):
+class MNISTModel(nn.Module):
     def __init__(self, input_shape: int, hidden_units: int, output_shape: int):
         super().__init__()
-        self.layer_stack = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(in_features=input_shape, out_features=hidden_units),
+        self.block1 = nn.Sequential(
+            nn.Conv2d(in_channels=input_shape, out_channels=hidden_units, kernel_size=3, padding=1),
             nn.ReLU(),
-            nn.Linear(in_features=hidden_units, out_features=output_shape),
-            nn.ReLU()
+            nn.Conv2d(in_channels=hidden_units, out_channels=hidden_units, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.block2 = nn.Sequential(
+            nn.Conv2d(in_channels=input_shape, out_channels=hidden_units, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=hidden_units, out_channels=hidden_units, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(in_features=hidden_units * 7 * 7, out_features=output_shape)
         )
     def forward(self, x):
-        return self.layer_stack(x)
+        x = self.block1(x)
+        x = self.block2(x)
+        x = self.classifier(x)
+        return x
 
-model_0 = FashionMNISTModel(input_shape=784, hidden_units=10, output_shape=len(class_names))
+model = MNISTModel(input_shape=784, hidden_units=10, output_shape=10)
 
 loss_fn = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(params=model_0.parameters(), lr=0.1)
-
-train_time_start_on_cpu = timer()
+optimizer = torch.optim.SGD(params=model.parameters(), lr=0.1)
 
 epochs = 3
 
-for epoch in tqdm(range(epochs)):
-    print(f"Epoch: {epoch}\n...")
-
+for epoch in range(epochs):
     train_loss = 0
     for batch, (X, y) in enumerate(train_dataloader):
-        model_0.train()
-
-        y_pred = model_0(X)
+        y_pred = model(X)
 
         loss = loss_fn(y_pred, y)
         train_loss += loss
@@ -126,29 +86,23 @@ for epoch in tqdm(range(epochs)):
         optimizer.step()
 
         if batch % 400 == 0:
-            print(f"Looked at {batch * len(X)} / {len(train_dataloader.dataset)} samples") #pyright: ignore
+            print(f"Looked at {batch * len(X)} /{len(train_dataloader.dataset)} samples") #pyright: ignore
 
     train_loss /= len(train_dataloader)
 
-    test_loss, test_acc = 0, 0
-    model_0.eval()
+    # Testing!!!
+    model.eval()
+
+    test_loss = 0
 
     with torch.inference_mode():
         for X, y in test_dataloader:
-            test_pred = model_0(X)
-
+            test_pred = model(X)
             test_loss += loss_fn(test_pred, y)
 
-            test_acc += accuracy_fn(y_true=y, y_pred=test_pred.argmax(dim=1))
+    test_loss /= len(test_dataloader)
 
-        test_loss /= len(test_dataloader)
-        test_acc /= len(test_dataloader)
+    print(f"Train loss: {train_loss} | Test loss: {test_loss}")
 
-    print(f"\nTrain loss: {train_loss:.5f} | Test loss: {test_loss:.5f}, Test acc: {test_acc:.2f}%\n")
 
-train_end_time_on_cpu = timer()
-
-total_train_time_model_0 = print_train_time(start=train_time_start_on_cpu, 
-                                           end=train_end_time_on_cpu,
-                                            device="cpu") #pyright: ignore
-
+print("Done!")
