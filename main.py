aa50a3044571bce43dbc6bbb9f431585abe0ca3b
squirrelcoding <rosilescarlos28@gmail.com>
2025-02-21 23:51:05 -0600

    dwf

--- main.py
+++ main.py
@@ -10,6 +10,13 @@ from torchvision.transforms import ToTensor
 
 import matplotlib.pyplot as plt
 
+# Calculate accuracy (a classification metric)
+def accuracy_fn(y_true, y_pred):
+    correct = torch.eq(y_true, y_pred).sum().item() # torch.eq() calculates where two tensors are equal
+    acc = (correct / len(y_pred)) * 100 
+    return acc
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
 train_data = datasets.MNIST(
     root="data",
@@ -27,6 +34,7 @@ test_data = datasets.MNIST(
     target_transform=None
 )
 
+
 train_dataloader = DataLoader(
     train_data,
     batch_size=32,
@@ -39,47 +47,69 @@ test_dataloader = DataLoader(
         shuffle=False
 )
 
+class_names = train_data.classes
+
 class MNISTModel(nn.Module):
     def __init__(self, input_shape: int, hidden_units: int, output_shape: int):
         super().__init__()
-        self.block1 = nn.Sequential(
-            nn.Conv2d(in_channels=input_shape, out_channels=hidden_units, kernel_size=3, padding=1),
+        self.block_1 = nn.Sequential(
+            nn.Conv2d(in_channels=input_shape, 
+                      out_channels=hidden_units, 
+                      kernel_size=3, # how big is the square that's going over the image?
+                      stride=1, # default
+                      padding=1),# options = "valid" (no padding) or "same" (output has same shape as input) or int for specific number 
             nn.ReLU(),
-            nn.Conv2d(in_channels=hidden_units, out_channels=hidden_units, kernel_size=3, padding=1),
+            nn.Conv2d(in_channels=hidden_units, 
+                      out_channels=hidden_units,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2)
+            nn.MaxPool2d(kernel_size=2,
+                         stride=2) # default stride value is same as kernel_size
         )
-        self.block2 = nn.Sequential(
-            nn.Conv2d(in_channels=input_shape, out_channels=hidden_units, kernel_size=3, padding=1),
+        self.block_2 = nn.Sequential(
+            nn.Conv2d(hidden_units, hidden_units, 3, padding=1),
             nn.ReLU(),
-            nn.Conv2d(in_channels=hidden_units, out_channels=hidden_units, kernel_size=3, padding=1),
+            nn.Conv2d(hidden_units, hidden_units, 3, padding=1),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2)
+            nn.MaxPool2d(2)
         )
         self.classifier = nn.Sequential(
             nn.Flatten(),
-            nn.Linear(in_features=hidden_units * 7 * 7, out_features=output_shape)
+            # Where did this in_features shape come from? 
+            # It's because each layer of our network compresses and changes the shape of our input data.
+            nn.Linear(in_features=hidden_units*7*7, 
+                      out_features=output_shape)
         )
-    def forward(self, x):
-        x = self.block1(x)
-        x = self.block2(x)
+    
+    def forward(self, x: torch.Tensor):
+        x = self.block_1(x)
+        # print(x.shape)
+        x = self.block_2(x)
+        # print(x.shape)
         x = self.classifier(x)
+        # print(x.shape)
         return x
 
-model = MNISTModel(input_shape=784, hidden_units=10, output_shape=10)
+
+model = MNISTModel(input_shape=1, hidden_units=10, output_shape=len(class_names)).to(device)
 
 loss_fn = nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(params=model.parameters(), lr=0.1)
 
-epochs = 3
+epochs = 0
 
 for epoch in range(epochs):
     train_loss = 0
+    train_acc = 0
     for batch, (X, y) in enumerate(train_dataloader):
+        X, y = X.to(device), y.to(device)
         y_pred = model(X)
 
         loss = loss_fn(y_pred, y)
         train_loss += loss
+        train_acc += accuracy_fn(y_true=y, y_pred=y_pred.argmax(dim=1))
 
         optimizer.zero_grad()
         loss.backward()
@@ -89,20 +119,41 @@ for epoch in range(epochs):
             print(f"Looked at {batch * len(X)} /{len(train_dataloader.dataset)} samples") #pyright: ignore
 
     train_loss /= len(train_dataloader)
+    train_acc /= len(train_dataloader)
 
     # Testing!!!
     model.eval()
 
     test_loss = 0
+    test_acc = 0
 
+    model.eval()
     with torch.inference_mode():
         for X, y in test_dataloader:
+            X, y = X.to(device), y.to(device)
             test_pred = model(X)
             test_loss += loss_fn(test_pred, y)
+            test_acc += accuracy_fn(y, test_pred.argmax(dim=1))
 
-    test_loss /= len(test_dataloader)
+        test_loss /= len(test_dataloader)
+        test_acc /= len(test_dataloader)
 
-    print(f"Train loss: {train_loss} | Test loss: {test_loss}")
+    print(f"Train loss: {train_loss}, Train accuracy: {train_acc} | Test loss: {test_loss}, Test accuracy{test_acc}")
+
+# Visualize some predictions!!!
+
+fig = plt.figure(figsize=(9, 9))
+rows, cols = 4, 4
+
+model.eval()
+
+with torch.inference_mode():
 
 
-print("Done!")
+    for i in range(1, rows * cols + 1):
+        random_idx = torch.randint(0, len(test_data), size=[1]).item()
+        img, label = test_data[random_idx] #pyright: ignore
+        fig.add_subplot(rows, cols, i)
+        plt.imshow(img.squeeze(), cmap="gray")
+        plt.title(class_names[label])
+        plt.axis(False);
